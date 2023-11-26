b2e355c8f944ce740ed1daf38343f29cd344028e
Sneara Parvin <130774997+Sneara0@users.noreply.github.com>
2023-11-26 11:27:55 +0600

    Add files via upload

---
+++ mid number.py
@@ -0,0 +1,18 @@
+x=int(input(" Enter 1st number"))
+y=int(input(" Enter 2nd number"))
+z=int(input(" Enter 3rd number"))
+
+if x>y:
+
+     if x>z:
+         print("x is bigger than y,z")
+     else:
+
+       print("z is bigger than x,y")
+
+elif y>x:
+
+       if y>z:
+           print("y is bigger than x,z")
+       else:
+           print("z is bigger than x,y")
